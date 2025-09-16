3f588a89e98ad9e2829ac7750a1b7c501523efe7
albert <alberix252@gmail.com>
2025-09-16 18:35:42 -0500

    primer commit del proyecto portafolio, se a realizado hasta la seccion portafolio

---
+++ js/main.js
@@ -0,0 +1,37 @@
+$(document).ready(function () {
+    $('.slider-portafolio').slick({
+        slidesToShow: 3,       // Número de cards visibles a la vez
+        slidesToScroll: 1,     // Cuántas se mueven por clic
+        infinite: true,        // Que sea infinito
+        arrows: true,          // Muestra flechas
+        dots: true,            // Muestra los puntitos de navegación
+        autoplay: true,        // Activa autoplay
+        autoplaySpeed: 3000,   // Velocidad del autoplay en ms
+        responsive: [
+            {
+                breakpoint: 1024, // Tablets y laptops pequeñas
+                settings: {
+                    slidesToShow: 2
+                }
+            },
+            {
+                breakpoint: 768, // Celulares grandes
+                settings: {
+                    slidesToShow: 1
+                }
+            }
+        ]
+    });
+});
+
+
+
+
+// Inicializa AOS cuando el DOM esté listo
+document.addEventListener("DOMContentLoaded", function () {
+    AOS.init({
+        duration: 1000, // duración de animaciones en ms
+        once: true,     // animar solo la primera vez
+        offset: 120,    // distancia desde el viewport para activar
+    });
+});
