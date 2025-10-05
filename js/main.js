33b7b81012dcf9301446d6acf4d6dd55c5717586
albert <alberix252@gmail.com>
2025-10-05 14:07:59 -0500

    corregi el error de enlace bootstrap js ya que no setvia el menu hamburguesa en las paginas

--- js/main.js
+++ js/main.js
@@ -55,3 +55,12 @@ $(document).ready(function () {
 
 });
 
+// Mantener tu efecto de scroll 
+window.addEventListener('scroll', function() {
+    const navbar = document.querySelector('.navbar');
+    if(window.scrollY > 50){
+        navbar.classList.add('scrolled');
+    } else {
+        navbar.classList.remove('scrolled');
+    }
+});
